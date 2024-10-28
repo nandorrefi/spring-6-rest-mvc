35e6512e95093bbd1129911313e84e4baa52b3af
NandorRefi <rnandor012@gmail.com>
2024-10-28 15:25:58 +0100

    POJO to JSON - ObjectMapper demonstration

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -1,5 +1,7 @@
 package guru.springframework.spring6restmvc.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.services.BeerService;
 import guru.springframework.spring6restmvc.services.BeerServiceImpl;
@@ -24,10 +26,20 @@ class BeerControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;  // WebMvcTest autoconfigures the Jackson ObjectMapper too initialized with default behavior
+
     @MockBean
     BeerService beerService;
 
     BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+
+    @Test
+    void testCreateNewBeer() throws JsonProcessingException {
+        Beer beer = beerServiceImpl.listBeers().getFirst();
+
+        System.out.println(objectMapper.writeValueAsString(beer));
+    }
 
     @Test
     void testListBeers() throws Exception {
