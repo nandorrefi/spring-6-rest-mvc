94bb9edde2d55a38b7a299b4bf8fafa218423558
NandorRefi <rnandor012@gmail.com>
2024-10-29 10:50:23 +0100

    Exception Handling - ResponseStatus annotation

--- src/main/java/guru/springframework/spring6restmvc/controller/NotFoundException.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/NotFoundException.java
@@ -1,5 +1,9 @@
 package guru.springframework.spring6restmvc.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Value Not Found")   // when the exception is thrown this response will be returned
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
     }
