fc446c8a0103cdcc24ecd1003169b92484dc6186
NandorRefi <rnandor012@gmail.com>
2024-10-29 12:22:01 +0100

    Setting up JPA repositories

---
+++ src/test/java/guru/springframework/spring6restmvc/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,27 @@
+package guru.springframework.spring6restmvc.repositories;
+
+import guru.springframework.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest    // test slice for Spring Data JPA components
+class CustomerRepositoryTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testSaveCustomer() {
+        Customer customer = customerRepository.save(
+                Customer.builder()
+                        .name("Customer Name")
+                        .build()
+        );
+
+        assertThat(customer.getId()).isNotNull();
+    }
+}
