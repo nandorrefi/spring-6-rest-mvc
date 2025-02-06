d7b94fbd28d00734d0b830f7da410c70800021ef
NandorRefi <rnandor012@gmail.com>
2025-02-06 01:24:18 +0100

    JPA Database Relationship Mappings - Persisting relational entities

---
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerOrderRepositoryTest.java
@@ -0,0 +1,45 @@
+package guru.springframework.spring6restmvc.repositories;
+
+import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.entities.BeerOrder;
+import guru.springframework.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest // so that bootstrap loads some test data for us
+class BeerOrderRepositoryTest {
+    @Autowired
+    BeerOrderRepository beerOrderRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    Customer testCustomer;
+    Beer testBeer;
+
+    @BeforeEach
+    void setUp() {
+        testCustomer = customerRepository.findAll().get(0);
+        testBeer = beerRepository.findAll().get(0);
+    }
+
+    @Transactional
+    @Test
+    void testBeerOrders() {
+        BeerOrder beerOrder = BeerOrder.builder()
+                .customerRef("Test order")
+                .customer(testCustomer)
+                .build();
+
+        BeerOrder savedBeerOrder = beerOrderRepository.saveAndFlush(beerOrder); // save object and persist to the database then return object from database
+        // with flushing (persisting to database) JPA can retrieve the relationship between objects, but constant flushing can lead to performance degradation
+
+        System.out.println(savedBeerOrder.getCustomerRef());
+    }
+}
